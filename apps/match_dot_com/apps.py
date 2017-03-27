84d2edf3e8c78523411ec4d386cc4c75d4326309
Edward Verity <eddieverity@gmail.com>
2017-03-27 12:08:43 -0500

    initial commit

---
+++ apps/match_dot_com/apps.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class MatchDotComConfig(AppConfig):
+    name = 'match_dot_com'
---
+++ apps/match_dot_com/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url, include
+from views import *
+
+urlpatterns = [
+    url(r'^$', index, name='index'),
+    url(r'^login$', login, name='login'),
+    url(r'^register$', register, name='register'),
+    url(r'^survey$', survey, name='survey'),
+
+]
