1959e9d88554e0be29a7cc2625d54901ed3b480b
Peter Herman <peter.b.herman@gmail.com>
2017-03-27 17:12:05 -0500

    changed routes and added styling

--- apps/match_dot_com/urls.py
+++ apps/match_dot_com/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url(r'^login$', login, name='login'),
     url(r'^register$', register, name='register'),
     url(r'^survey$', survey, name='survey'),
-
+    url(r'^logout$', logout, name='logout'),
 ]
