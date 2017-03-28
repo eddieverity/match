8865bce28e0cb5c65ba43c230d8dafacc7d6ed10
Edward Verity <eddieverity@gmail.com>
2017-03-28 13:44:46 -0500

    finished profile survey

--- apps/match_dot_com/views.py
+++ apps/match_dot_com/views.py
@@ -4,8 +4,10 @@ from django.core.exceptions import ValidationError
 from django.contrib import messages
 import re
 import bcrypt
+import math
 from models import *
 from django.db.models import Q
+
 
 # Create your views here.
 def index(request):
@@ -18,30 +20,144 @@ def index(request):
   return redirect ('match:login')
 
 def survey(request):
-  id = request.session['id']
+  if request.method=='POST':
+    my_id = request.session['id']
 
+    #request.POST.get('is_private', False);
+
+    user_gender = request.POST.get('user_gender', 'null')
+    user_age = request.POST['age']
+    
+    user_height_total = request.POST['valueA']
+    # user_height_in = request.POST['user_height_in']
+    # user_height_total = (height_ft*12)+height_in
+    user_body_type = request.POST['user_bodytype']
+    user_relationship_status = request.POST['user_status']
+    user_marriage = request.POST['user_marriage']
+    user_current_kids = request.POST['user_currkids']
+    user_future_kids = request.POST['user_futurekids']
+    user_education = request.POST['user_edu']
+    user_smoke = request.POST['user_smoke']
+    user_drink = request.POST['user_drink']
+
+    user_religion = request.POST['user_religion']
+    user_salary = request.POST['user_salary']
+
+    activity=0
+    frugality=0
+    pragmaticism=0
+    family=0
+
+    family+=int(user_current_kids)
+    family+=int(user_future_kids)
+    family+=int(user_marriage)
+    
+    user_interests = request.POST.getlist('user_interests')
+    for interest in user_interests:
+      if interest == 'book-club':
+        activity -=1
+        frugality -=1
+      if interest == 'camping':
+        activity +=1
+        frugality -=1
+        pragmaticism -=1
+      if interest == 'coffee':
+        activity -=1
+        frugality -=1
+      if interest == 'networking':
+        pragmaticism -=1
+        activity +=1
+      if interest == 'cooking':
+        activity -=1
+        frugality -=1
+        pragmaticism +=1
+      if interest == 'dining-out':
+        activity +=1
+        frugality +=1
+      if interest == 'fishing-hunting':
+        activity +=1
+        pragmaticism -=1
+        family -=1
+      if interest == 'gardening':
+        frugality -=1
+        pragmaticism +=1
+      if interest == 'hobbies':
+        pragmaticism +=1
+      if interest == 'movies':
+        activity -=1
+        frugality -=1
+      if interest == 'museums':
+        activity +=1
+        pragmaticism +=1
+      if interest == 'music':
+        pragmaticism +=1
+      if interest == 'exploring':
+        activity +=1
+        frugality +=1
+      if interest == 'nightclubs':
+        activity +=1
+        frugality +=1
+      if interest == 'performing-arts':
+        pragmaticism +=1
+      if interest == 'cards':
+        frugality+=1
+        activity-=1
+      if interest == 'playing-sports':
+        activity+=1
+      if interest == 'political':
+        pragmaticism-=1
+      if interest == 'shopping':
+        frugality+=1
+      if interest == 'travel':
+        frugality+=1
+        activity+=1
+      if interest == 'video-games':
+        activity-=1
+      if interest == 'volunteering':
+        activity+=1
+        family+=1
+      if interest == 'watching-sports':
+        activity-=1
+      if interest == 'wine':
+        frugality+=1
+        pragmaticism+=1
+
+    print user_height_total
+    print family
+    print frugality
+    print activity
+    print pragmaticism
+
+    
+
+    curr_user= Profile.objects.create(
+      user_id=my_id,
+      gender=user_gender,
+      age=user_age,
+      height=user_height_total,
+      body=user_body_type,
+      relationship_status=user_relationship_status,
+      current_kids=user_current_kids,
+      future_kids=user_future_kids,
+      education=user_education,
+      smoke=user_smoke,
+      drink=user_drink,
+      religion=user_religion,
+      salary=user_salary,
+      activity=activity,
+      frugality=frugality,
+      pragmaticism=pragmaticism,
+      family=family,
+      )
+
+    return redirect('match:survey_seeking')
+  return render(request, 'match_dot_com/survey.html')
+
+def survey_seeking(request):
+  return render(request, 'match_dot_com/survey_seeking.html')
+
+def seeking_entry(request):
   pass
-  user_gender = request.POST['user_gender']
-  user_age = request.POST['user_age']
-  
-  user_height_ft = request.POST['user_height_ft']
-  user_height_in = request.POST['user_height_in']
-  user_height_total = (height_ft*12)+height_in
-  user_body_type = request.POST['user_body_type']
-  user_relationship_status = request.POST['user_relationship_status']
-  user_current_kids = request.POST['user_current_kids']
-  user_future_kids = request.POST['user_future_kids']
-  user_education = request.POST['user_education']
-  user_smoke = request.POST['user_smoke']
-  user_drink = request.POST['user_drink']
-
-  user_religion = request.POST['user_religion']
-  user_salary = request.POST['user_salary']
-  user_interests = request.POST['user_interests']
-  #calculate Activity, Spending, Artistic, Family scales
-
-
-
   #Select matches
   seeking_gender = request.POST['seeking_gender']
 
@@ -80,15 +196,22 @@ def survey(request):
 
 
 def login(request):
+  
   if request.POST:
     l_email=request.POST['l_email']
+
     try:
       curr_user=User.objects.get(email = l_email)
       l_password=request.POST['l_password'].encode(encoding="utf-8")
       if bcrypt.hashpw(l_password, curr_user.password.encode("utf-8")) == curr_user.password:
         request.session['first_name']=curr_user.first_name
         request.session['id']=curr_user.id
-        return redirect('match:index')
+        try:
+          user_survey= Profile.objects.get(user_id=curr_user.id)
+          return redirect('match:index')
+        except:
+          return render(request, 'match_dot_com/survey.html')
+        
       else:
         messages.error(request, 'Incorrect password')
         return redirect('match:register')
@@ -134,7 +257,7 @@ def register(request):
         )
       request.session['first_name']=request.POST['first_name']
       request.session['id']=curr_user.id
-      return render (request, 'match_dot_com/survey.html')
+      return redirect ('match:survey')
     else:
       return redirect('match:login')
   return redirect('match:login')
