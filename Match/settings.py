56104d4de1df57617d3d4ed4da24a60eb57074de
Peter Herman <peter.b.herman@gmail.com>
2017-03-28 15:45:19 -0500

    more tweaks to image uploader and styling

--- Match/settings.py
+++ Match/settings.py
@@ -119,4 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_ROOT = 'apps/match_dot_com/static/match_dot_com/img'
+MEDIA_ROOT = 'apps/match_dot_com/static/match_dot_com'
